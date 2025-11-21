MashaKashirina/sailing-analytics

commit 5d594a47aae2751cc783d83e923ef934cf4a73f5

bug6060: moreLoginInfo popup for debranded is back + Solutions are now available both mobile/desktop in debranded

java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/authentication/login/LoginHintContent.java
@@ -70,10 +70,5 @@ public class LoginHintContent extends Composite {
                 }
             }
         });
-
-        if (!ClientConfiguration.getInstance().isBrandingActive()) {
-            moreInfo.getElement().getStyle().setDisplay(Display.NONE);
-        }
-
     }
 }

java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/desktop/partials/header/Header.java
@@ -374,7 +374,7 @@ public class Header extends Composite implements HeaderConstants {
             solutionsPageLink.getElement().getStyle().setDisplay(Display.NONE);
             logoAnchor.setHref("");
             logoAnchor.setTitle(StringMessages.INSTANCE.sailingAnalytics());
-            menuItemVisibilityHandler.addIgnore(solutionsPageLink);
+            //menuItemVisibilityHandler.addIgnore(solutionsPageLink);
         } else {
             logoAnchor.setHref(UriUtils.fromString(StringMessages.INSTANCE.sapAnalyticsURL()).asString());
         }
@@ -383,7 +383,6 @@ public class Header extends Composite implements HeaderConstants {
         subscriptionsPageLink.getElement().setAttribute(DEBUG_ID_ATTRIBUTE, "subscriptionsPageLink");
         logoAnchor.setAttribute(DEBUG_ID_ATTRIBUTE, "logoAnchor");
         eventsPageLink.getElement().setAttribute(DEBUG_ID_ATTRIBUTE, "eventsPage");
-        //remaining entries please see at registration for AuthenticationContextEvent 
         menuItemVisibilityHandler.refreshVisibilityDeferred();
     }
 

java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/desktop/partials/solutions/Solutions.gss
@@ -117,6 +117,7 @@
   background-repeat: no-repeat;
   background-position: left center;
   background-size: 100%;
+  background-image: resourceUrl("solutionsImage");
   padding: 4em 0 4.333333333333333em;
 }
 @media (min-width: 45em) {
@@ -138,6 +139,7 @@
   max-width: 88em;
   background-repeat: no-repeat;
   background-size: 40%;
+  background-image: resourceUrl("raceManagerImage");
   background-position: center right;
 }
 @media (min-width: 45em) {
@@ -159,6 +161,7 @@
   background-repeat: no-repeat;
   background-position: left center;
   background-size: 40%;
+  background-image: resourceUrl("sailingInsightImage");
   padding: 4em 0 4.333333333333333em;
 }
 @media (min-width: 45em) {
@@ -179,6 +182,7 @@
   padding: 4em 50% 4.333333333333333em 0;
   max-width: 88em;
   background-repeat: no-repeat;
+  background-image: resourceUrl("buoyPingerImage");
   background-size: 40%;
   background-position: center right;
 }
@@ -197,6 +201,7 @@
 }
 .solutions_contentsimulator .gridalternator {
   background-repeat: no-repeat;
+  background-image: resourceUrl("simulatorImage");
   background-position: left center;
   background-size: 100%;
   padding: 3.333333333333333em 0 3.333333333333333em;

java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/desktop/partials/solutions/Solutions.java
@@ -28,6 +28,7 @@ import com.sap.sailing.gwt.ui.client.StringMessages;
 import com.sap.sse.gwt.shared.ClientConfiguration;
 import com.google.gwt.dom.client.HeadingElement;
 import com.google.gwt.dom.client.ParagraphElement;
+import com.google.gwt.dom.client.Style.Display;
 
 public class Solutions extends Composite {
     interface SolutionsUiBinder extends UiBinder<Widget, Solutions> {
@@ -126,6 +127,25 @@ public class Solutions extends Composite {
             simulatorDivAlternator.getStyle().setBackgroundImage("url(\'"+ClientConfiguration.getInstance().getSailingSimulatorImageURL()+"\')");
             raceDivGridAlternator.getStyle().setBackgroundImage("url(\'"+ClientConfiguration.getInstance().getSailingRaceManagerAppImageURL()+"\')");
             buoyPingerDivAlternator.getStyle().setBackgroundImage("url(\'"+ClientConfiguration.getInstance().getBuoyPingerAppImageURL()+"\')");
+        } else {
+            inSailingAnchor.setVisible(false);
+            sailingAnalyticsAnchor.setText(i18n.solutionsAnalyticsHeadline(""));
+            raceAnchor.setText(i18n.solutionsRaceHeadline(""));
+            inSightAnchor.setText(i18n.sailInSightName());
+            buoyPingerAnchor.setText(i18n.solutionsBuoyPingerHeadline(""));
+            
+            
+            inSailingDiv.getStyle().setDisplay(Display.NONE);
+            sailingAnalyticsTitleHeader.setInnerText(i18n.sailingAnalyticsTitle(""));
+            contentSailingAnalytics1.setInnerText(i18n.contentSailingAnalytics1(""));
+            contentSailingAnalytics2.setInnerText(i18n.contentSailingAnalytics2(""));
+            sailingRaceManagerHeader.setInnerText(i18n.sailingRaceManager(""));
+            contentSailingRaceManager.setInnerText(i18n.contentSailingRaceManager(""));
+            sailInSightHeader.setInnerText(i18n.sailInSightName());
+            contentSailInSight.setInnerText(i18n.contentSailInSight(""));
+            sailingBuoyPingerHeader.setInnerText(i18n.sailingBuoyPinger(""));
+            contentSailingBuoyPinger.setInnerText(i18n.contentSailingBuoyPinger(""));
+            
         }
         sailingAnalyticsDetailsNavigation = placesNavigator.getWhatsNewNavigation(WhatsNewNavigationTabs.SailingAnalytics);
         raceCommitteeAppDetailsNavigation =  placesNavigator.getWhatsNewNavigation(WhatsNewNavigationTabs.RaceManagerApp);

java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/home/desktop/partials/solutions/SolutionsResources.java
@@ -4,12 +4,24 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.resources.client.CssResource.Shared;
+import com.google.gwt.resources.client.ImageResource;
 
 public interface SolutionsResources extends ClientBundle {
     public static final SolutionsResources INSTANCE = GWT.create(SolutionsResources.class);
 
     @Source("Solutions.gss")
     LocalCss css();
+    
+    @Source("solutions-sailing-insight.png")
+    ImageResource sailingInsightImage();
+    @Source("solutions-sailing-buoy-pinger.png")
+    ImageResource buoyPingerImage();
+    @Source("solutions-sailing-race-manager.png")
+    ImageResource raceManagerImage();
+    @Source("solutions-simulator.png")
+    ImageResource simulatorImage();
+    @Source("solutions.png")
+    ImageResource solutionsImage();
     
     @Shared
     public interface LocalCss extends CssResource {

commit ac7d02e77db56d921276c745c812ea4059afdf1b

bug6060 aftermath: remove unused imports caused by 5d594a47aae2751cc783d83e923ef934cf4a73f5

java/com.sap.sailing.dashboards.gwt/GWT Dashboards SDM.launch
@@ -51,7 +51,6 @@
     </listAttribute>
     <stringAttribute key="org.eclipse.jdt.launching.CLASSPATH_PROVIDER" value="com.gwtplugins.gwt.eclipse.core.moduleClasspathProvider"/>
     <booleanAttribute key="org.eclipse.jdt.launching.DEFAULT_CLASSPATH" value="false"/>
-    <stringAttribute key="org.eclipse.jdt.launching.JRE_CONTAINER" value="org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-17/"/>
     <stringAttribute key="org.eclipse.jdt.launching.MAIN_TYPE" value="com.google.gwt.dev.DevMode"/>
     <stringAttribute key="org.eclipse.jdt.launching.MODULE_NAME" value="com.sap.sailing.dashboards.gwt"/>
     <stringAttribute key="org.eclipse.jdt.launching.PROGRAM_ARGUMENTS" value="-style PRETTY -logLevel INFO -noserver -incremental -war &quot;${project_loc:com.sap.sailing.dashboards.gwt}&quot; -remoteUI &quot;${gwt_remote_ui_server_port}:${unique_id}&quot; -codeServerPort 9875 -startupUrl /dashboards/RibDashboard.html com.sap.sailing.dashboards.gwt.RibDashboard"/>

java/com.sap.sse.security.ui/src/main/java/com/sap/sse/security/ui/authentication/login/LoginHintContent.java
@@ -2,7 +2,6 @@ package com.sap.sse.security.ui.authentication.login;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
-import com.google.gwt.dom.client.Style.Display;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -13,7 +12,6 @@ import com.google.gwt.user.client.EventListener;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
-import com.sap.sse.gwt.shared.ClientConfiguration;
 
 /**
  * Widget that shows a message to inform the user that it has benefits to log in. There are two links the user can