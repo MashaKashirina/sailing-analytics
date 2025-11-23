a71500bbf1851b5f1d5551383011ef5215e5a14d
T Stokes - uni <thomasstokes@yahoo.co.uk>
2025-11-23 16:18:10 +0000

    bug6181: Rename variables and methods, and remove bulky if statements.

--- configuration/environments_scripts/central_reverse_proxy/files/home/wiki/app.rb
+++ configuration/environments_scripts/central_reverse_proxy/files/home/wiki/app.rb
@@ -31,29 +31,10 @@ class App < Precious::App
       if self.env['PATH_INFO'].split('/')[1] == 'gollum' && self.env['PATH_INFO'].split('/')[2] == 'assets'
         return
       end
-      @_auth =  Rack::Auth::Basic::Request.new(request.env)
-      if self.env['PATH_INFO'].split('/').length >=2 &&
-        (self.env['PATH_INFO'].split('/')[1] != 'wiki' &&
-        self.env['PATH_INFO'].split('/')[2] != 'wiki' &&
-        self.env['PATH_INFO'].split('/')[1] != 'home' &&
-        self.env['PATH_INFO'].split('/')[2] != 'home' &&
-        self.env['PATH_INFO'].split('/')[1] != 'Home' &&
-        self.env['PATH_INFO'].split('/')[2] != 'Home' &&
-        self.env['PATH_INFO'].split('/')[1] != 'Home.md' &&
-        self.env['PATH_INFO'].split('/')[1] != 'search' &&
-        self.env['PATH_INFO'].split('/')[2] != 'search' &&
-        self.env['PATH_INFO'].split('/')[1] != 'edit' &&
-        self.env['PATH_INFO'].split('/')[2] != 'edit' &&
-        self.env['PATH_INFO'].split('/')[1] != 'preview' &&
-        self.env['PATH_INFO'].split('/')[2] != 'preview' &&
-        (self.env['PATH_INFO'].split('/')[1] != 'gollum' ||
-          (self.env['PATH_INFO'].split('/')[2] != 'create' &&
-             (self.env['PATH_INFO'].split('/')[2] != 'overview' || self.env['PATH_INFO'].split('/')[3] != 'wiki'))))
+      @auth =  Rack::Auth::Basic::Request.new(request.env)
         throw(:halt, [403, 'Forbidden - You can not access anything outside wiki/ path.'])
       end
-      if @_auth.provided?
-      end
-      if @_auth.provided? && @_auth.basic? && @_auth.credentials && @user = detected_user(@_auth.credentials)
+      if @auth.provided? && @auth.basic? && @auth.credentials && (@user = get_user(@auth.credentials))
         Precious::App.set(:loggedInUser, @user.name)
         return @user
       else
@@ -70,6 +51,7 @@ class App < Precious::App
       @_users ||= settings.authorized_users.map {|u| User.new(*u) } # The ||= evalutes RHS only if left hand side is falsy.
     end
 
+    def get_user(credentials)
     def detected_user(credentials)
       users.detect do |u|
         [u.email, u.password_hash] ==
