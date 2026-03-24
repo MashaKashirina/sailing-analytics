527437c01d3f7d05d3cd1fcd8a0d2c4dea8ad527
Axel Uhl <axel.uhl@sap.com>
2026-03-24 10:30:23 +0100

    management-console event editing: removed unused import

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/managementconsole/places/event/edit/EditEventActivity.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/managementconsole/places/event/edit/EditEventActivity.java
@@ -2,7 +2,6 @@ package com.sap.sailing.gwt.managementconsole.places.event.edit;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
